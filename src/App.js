230f993bf132afb5b4f69a3a2a5fd4263f902f8f
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-02 12:13:01 +0300

    Fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState();
+
+  async function fetchData() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    setUser(data.results[0]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [loading]);
+
   return (
     <div className="App">
-
+      {loading ? (
+        <h1>loading</h1>
+      ) : (
+        <div>
+          <img src={user.picture.large} alt="user" />
+          <h1>Hi, I am {user.name.first + " " + user.name.last} from {user.location.country}.</h1>
+          <h2>I am {user.dob.age} years old.</h2>
+          <h3>My email adress is {user.email}</h3>
+          <button className="btn-change" onClick={() => setLoading(true)}>Change User</button>
+        </div>
+      )}
     </div>
   );
 }
